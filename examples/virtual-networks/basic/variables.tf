a3ad893a1631dcef354793e77848c8c8753add4f
Olumide Ajiboye <olumideajiboye@yahoo.com>
2019-06-01 13:29:17 +0100

    add variables definition

--- examples/virtual-networks/basic/variables.tf
+++ examples/virtual-networks/basic/variables.tf
@@ -1,9 +1,22 @@
 variable "prefix" {
-  description = "The prefix used for all resources in this example"
+  description = "The prefix used for all resources in this project"
   default = "vtltd"
 }
 
 variable "location" {
-  description = "The Azure location where all resources in this example should be created"
+  description = "The Azure location where all resources in this project should be created"
   default = "West Europe"
+}
+
+variable "access_key" {
+  description = "The Azure Storage Account Access Key for Terraform state backend"
+}
+
+variable "account_name" {
+  description = "The Azure Storage Account name for Terraform state backend"
+}
+
+variable "resource_group" {
+  description = "The Azure resource group where all resources in this project should be created"
+  default = "vtltd-rg"
 }
